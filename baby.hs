2eaaf251adebdd7188288fd1b4c92c391b4c798d
renatolrr <renatolrr@gmail.com>
2017-03-27 12:02:19 +0200

    lista intensional

--- baby.hs
+++ baby.hs
@@ -1,1 +1,8 @@
 doubleMe x = x + x
+doubleUs x y = x*2 + y*2
+
+boomBangs xs = [ if x < 10 then "Menor" else "Mayor" | x <- xs, odd x]
+
+length1 xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [c | c <- st, elem c ['A'..'Z']]
